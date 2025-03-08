d1942170207092db418bec7bce1fa31886412ce1
hanood_tunio <hanood.tunio@gmail.com>
2025-03-09 02:01:42 +0500

    added scaling 200

--- front-end/src/components/Cards.jsx
+++ front-end/src/components/Cards.jsx
@@ -1,6 +1,6 @@
 function Cards({ item }) {
   return (
-    <div className="card bg-base-100 w-92 shadow-sm my-3 p-3">
+    <div className="card bg-base-100 w-92 shadow-sm my-3 p-3 hover:scale-110 duration-200">
       <figure>
         <img src={item.Image} alt={item.nome} />
       </figure>
