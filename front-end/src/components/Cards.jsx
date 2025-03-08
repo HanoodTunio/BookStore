e6021c2b7b599ca5bd34ce61ef65647a72a53122
hanood_tunio <hanood.tunio@gmail.com>
2025-03-09 03:14:43 +0500

    dark mode

--- front-end/src/components/Cards.jsx
+++ front-end/src/components/Cards.jsx
@@ -1,18 +1,27 @@
 function Cards({ item }) {
   return (
-    <div className="card bg-base-100 w-92 shadow-sm my-3 p-3 hover:scale-110 duration-200">
+    <div className="card w-92 shadow-sm my-3 p-3 hover:scale-110 duration-200 border border-red-500 bg-white dark:bg-gray-800">
       <figure>
-        <img src={item.Image} alt={item.nome} />
+        <img src={item.Image} alt={item.name} />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          {item.nome}{" "}
-          <div className="badge badge-secondary">{item.category}</div>
+
+      <div className="card-body text-black dark:text-white">
+        <h2 className="card-title text-black dark:text-white">
+          {item.name}
+          <div className="badge badge-secondary text-black dark:text-white">
+            {item.category}
+          </div>
         </h2>
-        <p>{item.title} </p>
+
+        <p className="text-black dark:text-white">{item.title} </p>
+
         <div className="card-actions flex justify-between">
-          <div className="badge badge-outline">${item.price}</div>
-          <div className="cursor-pointer px-3 py-1 border border[2px] rounded-full hover:bg-pink-500 hover:text-white duration-200">
+          <div className="badge badge-outline text-black dark:text-white">
+            ${item.price}
+          </div>
+
+          {/* Ensure "Buy Now" button dynamically changes text color */}
+          <div className="cursor-pointer px-3 py-1 border-2 rounded-full border-gray-500 hover:bg-pink-500 hover:text-white duration-200 text-black dark:text-white">
             Buy Now
           </div>
         </div>
