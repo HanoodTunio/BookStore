614c1cd4f990fac028d98fee6a8e17e7cbfdec75
hanood_tunio <hanood.tunio@gmail.com>
2025-03-09 01:27:08 +0500

    Create Course page with NavBar

---
+++ front-end/src/components/Course.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function Course() {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default Course;
