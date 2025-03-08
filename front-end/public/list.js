e125374e692b9033cc043e6ee1166c17e2211b0c
hanood_tunio <hanood.tunio@gmail.com>
2025-03-09 00:12:59 +0500

    dummy books data

---
+++ front-end/public/list.js
@@ -0,0 +1,49 @@
+const books = [
+    {
+        "id": 1,
+        "nome": "Story Book",
+        "title": "Lorem, ipsum dollar sit emet consectetur adipisicing elit",
+        "price": 0,
+        "category": "Free",
+        "Image": "https://img.freepik.com/free-psd/black-book-mockup-elegant-simple-design-publication_191095-84101.jpg?t=st=1741433187~exp=1741436787~hmac=abb7ccecc86ceaecf13236cd56b322c2e18a1796bdc7fbfb79d1dc22666d6886&w=900"
+
+    },
+    {
+        "id": 2,
+        "nome": "Fictional Book",
+        "title": "Lorem, ipsum dollar sit emet consectetur adipisicing elit",
+        "price": 20,
+        "category": "Fictional",
+        "Image": "https://img.freepik.com/free-psd/black-book-mockup-elegant-simple-design-publication_191095-84101.jpg?t=st=1741433187~exp=1741436787~hmac=abb7ccecc86ceaecf13236cd56b322c2e18a1796bdc7fbfb79d1dc22666d6886&w=900"
+
+    },
+    {
+        "id": 3,
+        "nome": "Sports Book",
+        "title": "Lorem, ipsum dollar sit emet consectetur adipisicing elit",
+        "price": 40,
+        "category": "Sports",
+        "Image": "https://img.freepik.com/free-psd/black-book-mockup-elegant-simple-design-publication_191095-84101.jpg?t=st=1741433187~exp=1741436787~hmac=abb7ccecc86ceaecf13236cd56b322c2e18a1796bdc7fbfb79d1dc22666d6886&w=900"
+
+    },
+    {
+        "id": 4,
+        "nome": "History Book",
+        "title": "Lorem, ipsum dollar sit emet consectetur adipisicing elit",
+        "price": 80,
+        "category": "history",
+        "Image": "https://img.freepik.com/free-psd/black-book-mockup-elegant-simple-design-publication_191095-84101.jpg?t=st=1741433187~exp=1741436787~hmac=abb7ccecc86ceaecf13236cd56b322c2e18a1796bdc7fbfb79d1dc22666d6886&w=900"
+
+    },
+    {
+        "id": 5,
+        "nome": "Story Book",
+        "title": "Lorem, ipsum dollar sit emet consectetur adipisicing elit",
+        "price": 0,
+        "category": "Free",
+        "Image": "https://img.freepik.com/free-psd/black-book-mockup-elegant-simple-design-publication_191095-84101.jpg?t=st=1741433187~exp=1741436787~hmac=abb7ccecc86ceaecf13236cd56b322c2e18a1796bdc7fbfb79d1dc22666d6886&w=900"
+
+    },
+]
+
+export default books;
