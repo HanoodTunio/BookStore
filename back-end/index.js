da572e07731d6e3e3b3917ae5582a18f02f29733
hanood_tunio <hanood.tunio@gmail.com>
2025-03-10 00:56:27 +0500

    Initial Server setup

---
+++ back-end/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config(); // Load .env variables
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
